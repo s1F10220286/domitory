7654d0240556adc2f2a0d4c1ddc3167771b6ef92
Akira <s1f102202864@iniad.org>
2024-09-08 03:04:28 +0900

    12proに対応

---
+++ server/api/@openapi-1725726528415.ts
@@ -0,0 +1,10 @@
+import type { Methods as Methods0 } from './health'
+import type { Methods as Methods1 } from '.'
+import type { Methods as Methods2 } from './private'
+import type { Methods as Methods3 } from './private/me'
+import type { Methods as Methods4 } from './private/tasks/_taskId@string'
+import type { Methods as Methods5 } from './private/tasks/di'
+import type { Methods as Methods6 } from './private/tasks'
+import type { Methods as Methods7 } from './session'
+
+type AllMethods = [Methods0, Methods1, Methods2, Methods3, Methods4, Methods5, Methods6, Methods7]
